6c5089ddf41b5dc7f32b27bc11eb259214a8ce54
PalomaCardozo <palomacardozo88@gmail.com>
2026-02-03 09:26:26 +0100

    Enable basic game interaction with flip cards

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
 const flippedCard = document.querySelectorAll(".flip-card-inner");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  flippedCard.forEach((card) => {
-    card.addEventListener("click", () => {
-      card.classList.toggle("flipped");
+  if (lockBoard) return;
 
-      setTimeout(function () {
-        card.classList.remove("flipped");
-      }, 5000);
-    });
+  const card = this;
+  card.classList.add("flipped");
+
+  if (card === firstCard) return;
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = card;
+    return;
+  }
+
+  secondCard = card;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  if (isMatch) {
+    disableCards();
+  } else {
+    unflipCards();
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(function () {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function shuffle() {
+  flippedCard.forEach((card) => {
+    const randomPos = Math.floor(Math.random() * flippedCard.length);
+    card.style.order = randomPos;
   });
 }
 
-flipCard();
+shuffle();
+
+flippedCard.forEach((card) => card.addEventListener("click", flipCard));
