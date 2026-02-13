af4c48f52e786522a9c2b4592ade3caeb3674d82
irynalopatina <lopatina.iryna@gmail.com>
2026-02-13 19:32:11 +0100

    cleaned flipCard()

--- app/script.js
+++ app/script.js
@@ -144,7 +144,7 @@ function disableCards() {
     secondCard.removeEventListener("transitionend", handleTransitionEnd);
 
     const matchedCards = document.querySelectorAll(
-      ".flip-card-inner.matched"
+      ".flip-card-inner.matched",
     ).length;
 
     if (matchedCards === gameCards.length) {
@@ -157,7 +157,6 @@ function disableCards() {
 
   secondCard.addEventListener("transitionend", handleTransitionEnd);
 }
-
 
 function showWinner() {
   lockBoard = true;
@@ -172,6 +171,12 @@ function showWinner() {
 }
 
 function flipCard(card) {
+  if (
+    lockBoard ||
+    card === firstCard ||
+    card.classList.contains("flipped") ||
+    card.classList.contains("matched")
+  )
   if (
     lockBoard ||
     card === firstCard ||
